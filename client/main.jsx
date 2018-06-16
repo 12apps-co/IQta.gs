ce6bfb361f2bcb245db75534009c5dd97722112f
Francis Kim <hello@franciskim.co>
2018-06-16 11:10:49 +1000

    RIP v1

--- client/main.jsx
+++ client/main.jsx
@@ -4,5 +4,5 @@ import { render } from 'react-dom'
 
 Meteor.startup(() => {
     selectedTags = []
-    render(<App/>, document.getElementById('app'))
+    //render(<App/>, document.getElementById('app'))
 })
